4e6c9b1f34b346f9db19b963677fcc4073da2574
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-01-11 09:08:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.98%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     int findComplement(int num) 
     {
-        int a=1,num1=num;
-        while(num1>0)    
-        {
-            num1=num1>>1;
-            if(num1>0)
-                a=(a<<1)+1;
-        }
-        return num^a;
+        unsigned mask = ~0;
+        while(mask&num) 
+            mask=mask<<1;
+        return ~num^mask;
     }
 };
