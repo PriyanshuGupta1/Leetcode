b2da6847068ccbee5400917412bf595aab572ab6
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-01-25 09:22:59 +0530

    Time: 20 ms (97.58%), Space: 22.5 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,24 +1,25 @@
-class Solution {
+class Solution
+{
 public:
     bool validMountainArray(vector<int>& arr) 
     {
         int i;
-        if(arr.size()<=2)
+        if(arr.size()<=2)//the size of the mountain should be atleast 3 i.e 3 elements in array
             return false;
-        for( i=1;i<arr.size();i++)    
+        for(i=1;i<arr.size();i++)    
         {
-            if(arr[i]==arr[i-1])
+            if(arr[i]==arr[i-1])// we encountered a plateau
                 return false;
-            if(i==1 && arr[i]<arr[i-1])
+            else if(i==1 && arr[i]<arr[i-1])// decreasing size without any peak
                 return false;
-            if(arr[i]<arr[i-1])
+            else if(arr[i]<arr[i-1])//mountain dip is found
                 break;
         }
-        if(i==arr.size())
+        if(i==arr.size())//didnt find a peak element
             return false;
         for(;i<arr.size();i++)
         {
-            if(arr[i]>=arr[i-1])
+            if(arr[i]>=arr[i-1])// second peak found
                 return false;
         }
         return true;
