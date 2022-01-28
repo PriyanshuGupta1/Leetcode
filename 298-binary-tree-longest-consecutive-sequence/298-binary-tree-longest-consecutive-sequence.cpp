529532caab335f7040b58d1051594152c8296f47
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-01-28 12:41:49 +0530

    Time: 75 ms (12.77%), Space: 32.5 MB (54.15%) - LeetHub

--- 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.cpp
+++ 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.cpp
@@ -14,27 +14,27 @@ public:
     int dfs(TreeNode *root,int count)
     {
         if(root==NULL)
-            return 0;
+            return 0; 
         int left_seq=0,right_seq=0;
         if(root->left!=NULL)
         {
             if(root->left->val-root->val==1)
-                left_seq=dfs(root->left,count+1);
+                left_seq=dfs(root->left,count+1);//sequence is correct so incremented count+1
             else
-                left_seq=dfs(root->left,1);
+                left_seq=dfs(root->left,1);//sequence is broken so count is sent as one
         } 
         if(root->right!=NULL)
         {
             if(root->right->val-root->val==1)
-                right_seq=dfs(root->right,count+1);
+                right_seq=dfs(root->right,count+1);//sequence is correct so incremented count+1
             else
-                right_seq=dfs(root->right,1);
+                right_seq=dfs(root->right,1);//sequence is broken so count is sent as one
         }
         return max(max(left_seq,right_seq),count);
     }
     int longestConsecutive(TreeNode* root) 
     {
-        int count1=dfs(root,1);
-        return count1;
+        int longest_seq=dfs(root,1);
+        return longest_seq;
     }
 };
