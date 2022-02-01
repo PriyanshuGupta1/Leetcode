b73dd7e73a43f382895b44b0050c44cc59001540
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-02-01 17:36:30 +0530

    Time: 3 ms (84.05%), Space: 9.6 MB (5.15%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) 
-    {
-        int rowToSearch=-1;
-        for(int i=0;i<matrix.size();i++)    
-        {
-            rowToSearch=i;
-            if(matrix[i][0]>target)
-            {
-                i--;
-                rowToSearch=i;
-                break;
-            }
-            if(matrix[i][0]==target)
-                return true;
-        }
-        if(rowToSearch<0)
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r=matrix.size(),c=matrix[0].size();
+        int totalElements=r*c-1;
+        if(r==0)
             return false;
-        for(int j=0;j<matrix[0].size();j++)
+        int left=0,right=totalElements;
+        int middleElement,middleIndex;
+        while(left<=right)
         {
-            if(matrix[rowToSearch][j]==target)
+            middleIndex=(left+right)/2;
+            middleElement=matrix[middleIndex/c][middleIndex%c];
+            if(target==middleElement)
                 return true;
+            else
+            {
+                if(target<middleElement)
+                    right=middleIndex-1;
+                else
+                    left=middleIndex+1;
+            }
         }
         return false;
     }
+             
 };
+//     //Approach 1 in O(M+N) (where N is number of rows whereas M is no of coloumns
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) 
+//     {
+//         int rowToSearch=-1;
+//         for(int i=0;i<matrix.size();i++)    
+//         {
+//             rowToSearch=i;
+//             if(matrix[i][0]>target)
+//             {
+//                 i--;
+//                 rowToSearch=i; //trying to find a row head whose value is greater than the target
+//                 break;
+//             }
+//             if(matrix[i][0]==target) //if its present in the head we will return the value as true i.e found
+//                 return true;
+//         }
+//         if(rowToSearch<0)
+//             return false;
+//         for(int j=0;j<matrix[0].size();j++)
+//         {
+//             if(matrix[rowToSearch][j]==target)//the value is found in that row
+//                 return true;
+//         }
+//         return false; //if the value is not found throughout
+//     }
+// };
