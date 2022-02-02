8e89364005036a5a4340c9a20126314c5aba0002
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-02-03 00:54:29 +0530

    Time: 12 ms (87.76%), Space: 8.8 MB (42.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> ans;
-        vector<int> hash(26, 0);
+        vector<int> shash(26, 0);
         vector<int> phash(26, 0);
-        int window = p.size();
-        int len = s.size();
-        if(len < window)
+        int window=p.size();
+        int len=s.size();
+        if(len<window)
         {
             return ans;
         }
-        int left = 0,right = 0;
-        while(right < window)
+        int left=0,right=0;
+        while(right<window)
         {
-            phash[p[right] - 'a'] += 1;
-            hash[s[right] - 'a'] += 1;
+            phash[p[right]-'a']+=1;
+            shash[s[right]-'a']+=1;
             right++;
         }
-        right -=1;
-        while(right < len)
+        right-=1;
+        while(right<len)
         {
-            if(phash == hash)
-            {
+            if(phash==shash)
                 ans.push_back(left);
-            }
             right+=1;
             if(right != len)
-            {
-                hash[s[right] - 'a'] += 1;
-            }
-            hash[s[left] - 'a'] -=1 ;
+                shash[s[right] - 'a'] += 1;
+            shash[s[left] - 'a'] -=1 ;
             left += 1;
         }
         return ans;
