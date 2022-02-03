0b8e352e2a03206c81428efd3206b31ce079b6b4
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-02-03 19:32:49 +0530

    Time: 3 ms (93.03%), Space: 9.4 MB (93.67%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
+    // Approach 2: Using two pointer Approach
+    vector<int> twoSum(vector<int>& numbers, int target) 
     {
-        unordered_map <int,int> un_mp;
         vector <int> v;
-        for(int i=0;i<nums.size();i++)
+        int left=0,right=numbers.size()-1;
+        while(left<right)
         {
-            int findTarget=target-nums[i];
-            if(un_mp.find(findTarget)!=un_mp.end())
+            int curr_sum=numbers[left]+numbers[right];
+            if(curr_sum==target)
             {
-                cout<<i<<" "<<findTarget;
-                v.push_back(un_mp[findTarget]+1);
-                v.push_back(i+1);
+                v.push_back(left+1);
+                v.push_back(right+1);
                 return v;
             }
+            else if(curr_sum>target)
+                right--;
             else
-                un_mp[nums[i]]=i;
+                left++;
             
         }
         return v;
     }
 };
+//     Approach 1: using unordered map
+//     Time complexity :O(N^2)
+//     Space Complexity : O(N)
+//     vector<int> twoSum(vector<int>& nums, int target) 
+//     {
+//         unordered_map <int,int> un_mp;
+//         vector <int> v;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             int findTarget=target-nums[i];
+//             if(un_mp.find(findTarget)!=un_mp.end())
+//             {
+//                 cout<<i<<" "<<findTarget;
+//                 v.push_back(un_mp[findTarget]+1);
+//                 v.push_back(i+1);
+//                 return v;
+//             }
+//             else
+//                 un_mp[nums[i]]=i;
+            
+//         }
+//         return v;
+//     }
