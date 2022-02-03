d5f6432c28c5eaccee0e67649fe2139b035c2d31
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-02-03 20:26:36 +0530

    Time: 4 ms (88.59%), Space: 9.7 MB (45.26%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    // Approach 2: Using two pointer Approach
     vector<int> twoSum(vector<int>& numbers, int target) 
     {
         vector <int> v;
@@ -23,26 +22,3 @@ public:
         return v;
     }
 };
-//     Approach 1: using unordered map
-//     Time complexity :O(N^2)
-//     Space Complexity : O(N)
-//     vector<int> twoSum(vector<int>& nums, int target) 
-//     {
-//         unordered_map <int,int> un_mp;
-//         vector <int> v;
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             int findTarget=target-nums[i];
-//             if(un_mp.find(findTarget)!=un_mp.end())
-//             {
-//                 cout<<i<<" "<<findTarget;
-//                 v.push_back(un_mp[findTarget]+1);
-//                 v.push_back(i+1);
-//                 return v;
-//             }
-//             else
-//                 un_mp[nums[i]]=i;
-            
-//         }
-//         return v;
-//     }
