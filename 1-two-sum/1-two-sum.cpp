9c4d7d371a7397fe27607d3fc21506d1803b3613
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-02-03 16:54:59 +0530

    Time: 20 ms (57.02%), Space: 10.7 MB (51.27%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
-        vector <int> v;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        unordered_map<int,int> m;
         for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size();j++)
+            if(m.find(target-nums[i])!=m.end())
             {
-                if(nums[i]+nums[j]==target)
-                {
-                    v.push_back(i);
-                    v.push_back(j);
-                    break;
-                }
+                v.push_back(m[target-nums[i]]);
+                v.push_back(i);
+                return v;
             }
+            else
+                m[nums[i]]=i;
         }
-        return v;
+       return v;
     }
 };
+    // Approach 1(Brute Force Technique) 
+    // Time complexity: O(N^2)
+    // Space Complexity :O(1)
+    // vector<int> twoSum(vector<int>& nums, int target) 
+    // {
+    //     vector <int> v;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         for(int j=i+1;j<nums.size();j++)
+    //         {
+    //             if(nums[i]+nums[j]==target)
+    //             {
+    //                 v.push_back(i);
+    //                 v.push_back(j);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return v;
+    // }
