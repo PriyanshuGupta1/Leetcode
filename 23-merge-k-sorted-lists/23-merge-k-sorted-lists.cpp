360f3882f3fd2272be667b56e38c5e7a72e6a96a
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-02-05 12:19:45 +0530

    Time: 32 ms (57.48%), Space: 14 MB (15.40%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,17 +8,20 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
+class Solution 
+{
+    public:
+    //Time Complexity: O(N(logN))
+    //Space Complexity :O(N)
     ListNode* mergeKLists(vector<ListNode*>& lists) 
     {
-        vector <int> a;
-        if(lists.size()==0)
+        vector <int> a; //creating a vector of array to store all nodes value
+        if(lists.size()==0) //if no lists are present just return NULL
             return NULL;
         for(int i=0;i!=lists.size();i++)
         {
-            ListNode *curr=lists[i];
-            for(auto j=0;;j++)
+            ListNode *curr=lists[i]; // accessing the first list
+            while(1)
             {
                 if(curr!=NULL)
                 {
@@ -34,7 +37,6 @@ public:
         for(int i=0;i<a.size();i++)
         {
             ListNode *curr1=new ListNode(a[i]);
-            cout<<a[i]<<" ";
             if(start==NULL)
             {
                 start=curr1;
