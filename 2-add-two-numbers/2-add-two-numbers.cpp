f62735ce82028f4d2c6974d25d6d909150bd03c9
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-03-10 08:22:06 +0530

    Time: 64 ms (29.30%), Space: 71.5 MB (11.57%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -8,63 +8,29 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-    long long int count=1,sum=0;
+class Solution 
+{
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
     {
-        ListNode *head=NULL,*prev=NULL;  
-        if(l1==NULL)
-            return l2;
-        if(l2==NULL)
-            return l1;
+        ListNode *head=new ListNode(0),*dummy=head;  
+        if(l1==NULL)return l2;
+        if(l2==NULL)return l1;
         int carry=0;
-        while(l1!=NULL && l2!=NULL)
+        while(l1!=NULL || l2!=NULL)
         {
-            ListNode *curr=new ListNode((carry+l1->val+l2->val)%10);
-            if(carry+l1->val+l2->val>=10)
-                carry=1;
-            else
-                carry=0;
-            if(head==NULL)
-            {
-                head=curr;
-                prev=curr;
-            }
-            else
-            {
-                prev->next=curr;
-                prev=curr;
-            }
-            l1=l1->next;
-            l2=l2->next;
-        }
-        if(l2!=NULL)
-            l1=l2;
-        while(l1!=NULL)
-        {
-            ListNode *curr=new ListNode((carry+l1->val)%10);
-            if(carry+l1->val>=10)
-                carry=1;
-            else
-                carry=0;
-            if(head==NULL)
-            {
-                head=curr;
-                prev=curr;
-            }
-            else
-            {
-                prev->next=curr;
-                prev=curr;
-            }
-            l1=l1->next;
+            int x=(l1!=NULL) ? l1->val:0;
+            int y=(l2!=NULL) ? l2->val:0;
+            head->next=new ListNode((carry+x+y)%10);
+            head=head->next;
+            carry=(carry+x+y)/10;
+            if(l1!=NULL) 
+                l1=l1->next;
+            if(l2!=NULL)
+                l2=l2->next;
         }
         if(carry==1)
-        {
-            ListNode *curr=new ListNode(carry);
-            prev->next=curr;
-        }
-        return head;
+            head->next=new ListNode(carry);
+        return dummy->next;
     }
 };
