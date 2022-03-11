415736ac417955478b93aeacdb0359e3634f6394
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-03-11 09:48:10 +0530

    Time: 16 ms (22.86%), Space: 11.8 MB (55.87%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,32 +10,48 @@
  */
 class Solution {
 public:
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
     ListNode* rotateRight(ListNode* head, int k) 
     {
-        if(head==NULL || head->next==NULL)    
+        if(head == NULL || head->next == NULL)    
             return head;
-        int len=0;
-        ListNode *cur=head;
-        while(cur!=NULL)
-        {
-            cur=cur->next;
-            len++;
-        }
-        k=k%len;
-        while(k--)
-        {
-            ListNode *curr=head,*prev=NULL;
-            while(curr->next!=NULL)
-            {
-                prev=curr;
-                curr=curr->next;
-            }
-            curr->next=head;
-            head=prev->next;
-            prev->next=NULL;
-        }
+        int len = 1;
+        ListNode *curr = head;
+        for(;curr->next != NULL; len++) // calculating the length of the list
+            curr = curr->next;
+        curr->next = head;// forming a ring by connecting it with the head
+        k=k%len;// for high value of k it will be shortened by taking mod
+        for(int i = 0 ;i <= len-k-1 ; i++)
+            curr = curr->next; // traversing to n-k-1 which will be the new last node
+        head = curr->next; // the new head of the list
+        curr->next = NULL; // the last node
         return head;
     }
 };
-// 1 2 3 4 5
-// k=2 1 2 3 4(prev) 5(curr)
+    // Time Complexity: O(N*k)
+    // Space Complexity: O(1)
+    // ListNode* rotateRight(ListNode* head, int k) 
+    // {
+    //     if(head==NULL || head->next==NULL)    
+    //         return head;
+    //     int len=0;
+    //     ListNode *curr=head;
+    //     for(;curr!=NULL;len++)
+    //         curr=curr->next;
+    //     k%=len;
+    //     curr=head;
+    //     while(k--)
+    //     {
+    //         ListNode *prev=NULL;
+    //         while(curr->next!=NULL)
+    //         {
+    //             prev=curr;
+    //             curr=curr->next;
+    //         }
+    //         curr->next=head;
+    //         head=prev->next;
+    //         prev->next=NULL;
+    //     }
+    //     return head;
+    // }
