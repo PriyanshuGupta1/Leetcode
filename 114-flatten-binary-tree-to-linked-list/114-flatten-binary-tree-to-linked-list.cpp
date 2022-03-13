5c8d77b60015c023f90baf459a4130f74a5741ba
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-03-13 16:42:41 +0530

    Time: 3 ms (94.75%), Space: 12.9 MB (12.79%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -11,15 +11,48 @@
  */
 class Solution {
 public:
-    TreeNode *prev=NULL;
     void flatten(TreeNode* root) 
     {
         if(root==NULL)
             return;
-        flatten(root->right);
-        flatten(root->left);
-        root->right=prev;
-        root->left=NULL;
-        prev=root;
+        stack <TreeNode *> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode *curr=st.top();
+            st.pop();
+            if(curr->right!=NULL)
+                st.push(curr->right);
+            if(curr->left!=NULL)
+                st.push(curr->left);
+            if(st.size()>=1)
+                curr->right=st.top();
+            curr->left=NULL;
+        }
     }
 };
+//     Recursive Solution
+//     Time Complexity: O(N)
+//     Space Complexity: O(H) :(height is max height of binary tree)
+//     TreeNode *prev=NULL;
+//     void flatten(TreeNode* root) 
+//     {
+//         if(root==NULL)
+//             return;
+//         flatten(root->right);
+//         flatten(root->left);
+//         root->right=prev;
+//         root->left=NULL;
+//         prev=root;
+//     }
+//Similar question but making the right pointer as NULL and left pointer as according to preorder traversal
+//     void flatten(TreeNode* root) 
+//     {
+//         if(root==NULL)
+//             return;
+//         flatten(root->right);
+//         flatten(root->left);
+//         root->left=prev;
+//         root->right=NULL;
+//         prev=root;
+//     }
