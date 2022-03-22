b4241e6f1bec2eb3da3c88fb5f0d8d3b5bd75ee1
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-03-22 10:31:56 +0530

    Time: 103 ms (47.23%), Space: 16.5 MB (96.89%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     int jump(vector<int>& nums) 
     {
-        int steps=0,last=nums.size()-1,curr_max=nums.size()-1,j=nums.size()-1;
-        while(j>0)
+        int steps=0,last=nums.size()-1,curr_max=nums.size()-1;
+        while(last>0)
         {
             for(int i=last-1;i>=0;i--)
-            {
                 if(i+nums[i]>=last && i<curr_max)
-                {
                     curr_max=i;
-                    j=i;
-                }
-            }    
-            
-            last=j;
-
+            last=curr_max;
             steps++;
-            curr_max=j;
         }
         return steps;
     }
