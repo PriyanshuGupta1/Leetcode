9150b9180de0a545acfcabe1d8ae0128ab85d0ad
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-03-26 18:09:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.76%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+    int calc(int n,vector <int> &dp)
+    {
+        if(n<=1)
+            return n;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=calc(n-1,dp)+calc(n-2,dp);
+    }
     int fib(int n) 
     {
-        if(n==0)    
-            return 0;
-        if(n==1)
-            return 1;
-        return fib(n-1)+fib(n-2);
+        vector <int> dp(n+1,-1);
+        return dp[n]=calc(n,dp);
     }
 };
