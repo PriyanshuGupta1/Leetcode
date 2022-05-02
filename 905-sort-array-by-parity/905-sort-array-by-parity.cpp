41b35b53c9a44086dfac4484300cea7defce67a1
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-05-02 10:46:01 +0530

    Time: 15 ms (47.89%), Space: 16.3 MB (57.28%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -4,13 +4,10 @@ public:
         int i=0,j=nums.size()-1;
         while(i<=j)
         {
-            if(nums[i]&1==1)
-            {
-                swap(nums[j],nums[i]);
-                j--;
-            }
-            else
-                i++;
+            if(nums[i]%2>nums[j]%2)
+                swap(nums[i],nums[j]);
+            if(nums[i]%2==0)i++;
+            if(nums[j]%2==1)j--;
         }
         return nums;
     }
