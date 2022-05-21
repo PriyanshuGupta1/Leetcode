e9cf53c284725ef65fb12461ef7523d424bf3bb3
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-05-21 17:01:50 +0530

    Time: 99 ms (91.80%), Space: 20.6 MB (74.18%) - LeetHub

--- 361-bomb-enemy/361-bomb-enemy.cpp
+++ 361-bomb-enemy/361-bomb-enemy.cpp
@@ -3,52 +3,39 @@ public:
     int maxKilledEnemies(vector<vector<char>>& grid) {
         int n=grid.size(),m=grid[0].size();
         int maxi=0;
+        int rowHit=0;
+        vector <int> colHit(m,0);
         for(int i=0;i<n;i++)
         {
-            int rowHit=-1;
             for(int j=0;j<m;j++)
             {
-                int colHit=0;
-                if(grid[i][j]=='0' && rowHit==-1)
+                if(j==0 || grid[i][j-1]=='W')
                 {
                     rowHit=0;
-                    for(int k=j-1;k>=0;k--)
+                    for(int k=j;k<m;k++)
                     {
-                        if(grid[i][k]=='E')
-                            rowHit++;
                         if(grid[i][k]=='W')
                             break;
-                    }
-                    for(int k=j+1;k<m;k++)
-                    {
-                        if(grid[i][k]=='E')
+                        else if (grid[i][k]=='E')
                             rowHit++;
-                        if(grid[i][k]=='W')
-                            break;
                     }
                 }
-                if(grid[i][j]=='0' && rowHit!=-1)
+                if(i==0 || grid[i-1][j]=='W')
                 {
-                    for(int k=i-1;k>=0;k--)
+                    colHit[j]=0;
+                    for(int k=i;k<n;k++)
                     {
-                        if(grid[k][j]=='E')
-                            colHit++;
                         if(grid[k][j]=='W')
                             break;
+                        else if(grid[k][j]=='E')
+                            colHit[j]++; 
                     }
-                    for(int k=i+1;k<n;k++)
-                    {
-                        if(grid[k][j]=='E')
-                            colHit++;
-                        if(grid[k][j]=='W')
-                            break;
-                    }
-                    maxi=max(maxi,rowHit+colHit);
                 }
-                if(grid[i][j]=='W')
-                    rowHit=-1;
+                if(grid[i][j]=='0')
+                {
+                    maxi=max(maxi,rowHit+colHit[j]);
+                }
             }
-            
         }
         return maxi;
     }
