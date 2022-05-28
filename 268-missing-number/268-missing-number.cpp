b689d405a8b341ffbfa55e0a7445aa4c2333dfee
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-05-28 09:44:24 +0530

    Time: 85 ms (5.13%), Space: 25.8 MB (5.21%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        map <int,int> mp;
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]++;
+        for(int i=0;i<nums.size();i++)
+            if(mp[i]==0)
+                return i;
+        return nums.size();
+    }
+};
