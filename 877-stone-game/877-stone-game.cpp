e9b3c315f8b90b17d3e5b2584f43e7d0f55f44f3
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-05-31 11:41:32 +0530

    Time: 599 ms (5.02%), Space: 116.7 MB (5.08%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    // 3 7 2 3
-    int recursion(int i,int j,int counter,vector <int> &piles,vector <vector <vector <int>>>&dp)
-    {
-        if(i>=j)
-            return 0;
-        if(dp[i][j][counter]!=-1)
-            return dp[i][j][counter];
-        if(counter)
-        {
-            return dp[i][j][counter]=max(piles[j]+recursion(i,j-1,0,piles,dp),piles[i]+recursion(i+1,j,0,piles,dp));
-        }
-        else
-        {
-            return dp[i][j][counter]=max(-piles[j]+recursion(i,j-1,1,piles,dp),-piles[i]+recursion(i+1,j,1,piles,dp));
-        }
-    }
     bool stoneGame(vector<int>& piles) {
-        vector <vector <vector <int>>> dp (piles.size(),vector <vector <int>>(piles.size(),vector <int>(2,-1)));
-        // vector <vector <vector <int>>> dp(prices.size(),vector <vector <int>>(2,vector <int>(3,-1)));
-        return recursion(0,piles.size()-1,1,piles,dp)>0 ? 1 : 0;
+        int n=piles.size();
+        vector <vector <vector <int>>> dp (n+1,vector <vector <int>>(n+1,vector <int>(2,0)));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                for(int counter=0;counter<2;counter++)
+                {
+                    if(i>=j)
+                        dp[i][j][counter]=0;
+                    else
+                    {
+                        if(counter==1)
+                        {
+                            dp[i][j][counter]=max(piles[j]+dp[i][j-1][0],piles[i]+dp[i+1][j][0]);
+                        }
+                        else
+                        {
+                            dp[i][j][counter]=max(-piles[j]+dp[i][j-1][1],-piles[i]+dp[i][j-1][1]);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][n-1][1]>0 ? 1 : 0;
     }
 };
