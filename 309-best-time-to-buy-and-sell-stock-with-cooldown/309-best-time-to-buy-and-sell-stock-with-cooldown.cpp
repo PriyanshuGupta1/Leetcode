3690eb7ab7297e7d46b8d30094ed0df7fcb510c0
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-05-31 16:12:39 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (46.39%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -12,7 +12,18 @@ public:
             return dp[index][buy]=max(prices[index]+recursion(index+2,1,prices,dp),recursion(index+1,buy,prices,dp));
     }
     int maxProfit(vector<int>& prices) {
-        vector <vector <int>> dp(prices.size(),vector <int>(2,-1));
-        return recursion(0,1,prices,dp);
+        vector <vector <int>> dp(prices.size()+2,vector <int>(2,0));
+        for(int index=prices.size()-1;index>=0;index--)
+        {
+            for(int buy=0;buy<2;buy++)
+            {
+                if(buy)
+                    dp[index][buy]=max(-prices[index]+dp[index+1][0],dp[index+1][buy]);
+                else
+                    dp[index][buy]=max(prices[index]+dp[index+2][1],dp[index+1][buy]);
+            }
+        }
+        
+        return dp[0][1];
     }
 };
