911e7fb63525f40915d301cd4b174a2cc67c216b
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-05-31 16:41:06 +0530

    Time: 288 ms (19.12%), Space: 90.8 MB (31.72%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    int recursion(int index,int buy,vector <int> &prices,int fee,vector <vector <int>> &dp)
-    {
-        if(index==prices.size())
-            return 0;
-        if(dp[index][buy] != -1)
-            return dp[index][buy];
-        if(buy)
-        {
-            return dp[index][buy]=max(-prices[index]+recursion(index+1,0,prices,fee,dp),0+recursion(index+1,buy,prices,fee,dp));
-        }
-        else
-        {
-            return dp[index][buy]=max(prices[index]-fee+recursion(index+1,1,prices,fee,dp),0+recursion(index+1,buy,prices,fee,dp));
-        }
-    }
+    // int recursion(int index,int buy,vector <int> &prices,int fee,vector <vector <int>> &dp)
+    // {
+    //     if(index==prices.size())
+    //         return 0;
+    //     if(dp[index][buy] != -1)
+    //         return dp[index][buy];
+    //     if(buy)
+    //     {
+    //         return dp[index][buy]=max(-prices[index]+recursion(index+1,0,prices,fee,dp),0+recursion(index+1,buy,prices,fee,dp));
+    //     }
+    //     else
+    //     {
+    //         return dp[index][buy]=max(prices[index]-fee+recursion(index+1,1,prices,fee,dp),0+recursion(index+1,buy,prices,fee,dp));
+    //     }
+    // }
     int maxProfit(vector<int>& prices, int fee) {
-        vector <vector <int>> dp(prices.size()+1,vector <int>(2,-1));
-        return recursion(0,1,prices,fee,dp);
+        vector <vector <int>> dp(prices.size()+1,vector <int>(3,0));
+        for(int index=prices.size()-1;index>=0;index--)
+        {
+            for(int buy=0;buy<2;buy++)
+            {
+                if(buy)
+                     dp[index][buy]=max(-prices[index]+dp[index+1][0],0+dp[index+1][buy]);
+                else
+                     dp[index][buy]=max(prices[index]-fee+dp[index+1][1],0+dp[index+1][buy]);
+            }
+        }
+        return dp[0][1];
     }
 };
