706e4ef0dc7007e64451ef716da48b8ba7ebdd5e
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-06-05 01:02:30 +0530

    Time: 9 ms (59.96%), Space: 7.3 MB (67.79%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    std::vector<std::vector<std::string> > solveNQueens(int n) {
+        std::vector<std::vector<std::string> > res;
+        std::vector<std::string> nQueens(n, std::string(n, '.'));
+        std::vector<int> flag(5 * n - 2, 1);
+        solveNQueens(res, nQueens, flag, 0, n);
+        return res;
+    }
+private:
+    void solveNQueens(std::vector<std::vector<std::string> > &res, std::vector<std::string> &nQueens, std::vector<int> &flag, int row, int &n) {
+        if (row == n) {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (flag[col] && flag[n + row + col] && flag[4 * n - 2 + col - row]) {
+                flag[col] = flag[n + row + col] = flag[4 * n - 2 + col - row] = 0;
+                nQueens[row][col] = 'Q';
+                solveNQueens(res, nQueens, flag, row + 1, n);
+                nQueens[row][col] = '.';
+                flag[col] = flag[n + row + col] = flag[4 * n - 2 + col - row] = 1;
+            }
+    }
+};
