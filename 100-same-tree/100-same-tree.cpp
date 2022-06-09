2613a38466f6720059e21a2c48da5c30ab5f15c2
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-06-09 11:31:11 +0530

    Time: 7 ms (17.92%), Space: 10.1 MB (7.14%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -20,9 +20,7 @@ public:
         if(p->val!=q->val)
             return false;
         else
-        {
             return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-        }
         return false;
     }
 };
