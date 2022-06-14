ea8754a14616f98b337777725fa209c129d91d03
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-06-14 09:36:30 +0530

    Time: 321 ms (9.72%), Space: 7.4 MB (66.67%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int recursion(int counter,int start,int end,vector <int> & nums)
+    {
+        if(start>end)
+            return 0;
+        if(counter%2==1)
+        {
+            return max(nums[start]+recursion(counter+1,start+1,end,nums),nums[end]+recursion(counter+1,start,end-1,nums));
+        }
+        else
+        {
+            return min(-nums[start]+recursion(counter+1,start+1,end,nums),-nums[end]+recursion(counter+1,start,end-1,nums));
+        }
+    }
+    bool PredictTheWinner(vector<int>& nums) {
+        return recursion(1,0,nums.size()-1,nums)>=0 ? 1:0;
+    }
+};
