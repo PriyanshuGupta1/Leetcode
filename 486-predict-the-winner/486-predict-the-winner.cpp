3bec72a46866adda074d16dbadb340e7849f5fc3
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-06-14 09:48:46 +0530

    Time: 6 ms (50.72%), Space: 7.7 MB (33.94%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int recursion(int counter,int start,int end,vector <int> & nums)
+    int recursion(int start,int end,vector <int> & nums,vector <vector <int>> &dp)
     {
         if(start>end)
             return 0;
-        if(counter%2==1)
-            return max(nums[start]+recursion(counter+1,start+1,end,nums),nums[end]+recursion(counter+1,start,end-1,nums));
-        else
-           return min(-nums[start]+recursion(counter+1,start+1,end,nums),-nums[end]+recursion(counter+1,start,end-1,nums));
+        if(dp[start][end] != -1)
+            return dp[start][end];
+        int a=nums[start]-recursion(start+1,end,nums,dp);
+        int b=nums[end]-recursion(start,end-1,nums,dp);
+        return dp[start][end]=max(a,b);
     }
     bool PredictTheWinner(vector<int>& nums) {
-        return recursion(1,0,nums.size()-1,nums)>=0 ? 1:0;
+        vector <vector <int>> dp(nums.size(),vector <int>(nums.size(),-1));
+        return recursion(0,nums.size()-1,nums,dp)>=0 ? 1:0;
     }
 };
