b332b75f272e27e4a0ea467b987991a0f75ec427
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-06-20 14:39:49 +0530

    Time: 3 ms (95.18%), Space: 10.9 MB (36.89%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector <vector <int>> res;
-    void recursion(int index,vector <int> &candidates,int target,vector <int> t)
+    vector <int> t;
+    void recursion(int index,vector <int> &candidates,int target)
     {
         if(target==0)
         {
@@ -15,14 +16,13 @@ public:
             if(candidates[index]>target)
                 break;
             t.push_back(candidates[i]);
-            recursion(i+1,candidates,target-candidates[i],t);
+            recursion(i+1,candidates,target-candidates[i]);
             t.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target)    {
-        vector <int> t;
         sort(candidates.begin(),candidates.end());
-        recursion(0,candidates,target,t);
+        recursion(0,candidates,target);
         return res;
     }
 };
