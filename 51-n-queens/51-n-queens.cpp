218c4f9629d7b8db25d695dacea49c54cd1edf32
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-06-21 09:40:18 +0530

    Time: 72 ms (14.53%), Space: 50.5 MB (5.26%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,25 +1,71 @@
 class Solution {
 public:
-    std::vector<std::vector<std::string> > solveNQueens(int n) {
-        std::vector<std::vector<std::string> > res;
-        std::vector<std::string> nQueens(n, std::string(n, '.'));
-        std::vector<int> flag(5 * n - 2, 1);
-        solveNQueens(res, nQueens, flag, 0, n);
-        return res;
+    bool isSafe(int row,int col,vector <string> board)
+    {
+        int i=row,j=col;
+        
+        while(i>=0 && j>=0)
+        {
+            if(board[i][j]=='Q')
+                return false;
+            i--;
+            j--;
+        }
+        i=row,j=col;
+        
+        while(j>=0)
+        {
+            if(board[i][j]=='Q')
+                return false;
+            j--;
+        }
+        i=row,j=col;
+        
+        while(i<board.size() && j>=0)
+        {
+            if(board[i][j]=='Q')
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
-private:
-    void solveNQueens(std::vector<std::vector<std::string> > &res, std::vector<std::string> &nQueens, std::vector<int> &flag, int row, int &n) {
-        if (row == n) {
-            res.push_back(nQueens);
+    void recursion(int col,vector <vector <string>> &res,vector <string> board)
+    {
+        if(col==board.size())
+        {
+            res.push_back(board);
+            // cout<<col<<" "<<board.size();
+            for(int i=0;i<board.size();i++)
+            {
+                for(int j=0;j<board.size();j++)
+                {
+                    cout<<board[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+            cout<<endl;
             return;
         }
-        for (int col = 0; col != n; ++col)
-            if (flag[col] && flag[n + row + col] && flag[4 * n - 2 + col - row]) {
-                flag[col] = flag[n + row + col] = flag[4 * n - 2 + col - row] = 0;
-                nQueens[row][col] = 'Q';
-                solveNQueens(res, nQueens, flag, row + 1, n);
-                nQueens[row][col] = '.';
-                flag[col] = flag[n + row + col] = flag[4 * n - 2 + col - row] = 1;
+        
+        for(int i=0;i<board.size();i++)
+        {
+            // cout<<col<<" "<<i;
+            if(isSafe(i,col,board))
+            {
+                board[i][col]='Q';
+                recursion(col+1,res,board);
+                board[i][col]='.';
             }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector <vector <string>> res;
+        vector <string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)
+            board[i]=s;
+        recursion(0,res,board);
+        return res;
     }
 };
