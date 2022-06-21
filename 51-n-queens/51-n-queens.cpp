0b528c7a0d88fab72a4be20afb5bb9ccb8720cb1
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-06-21 09:53:13 +0530

    Time: 73 ms (14.22%), Space: 11.9 MB (21.60%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -30,7 +30,7 @@ public:
         }
         return true;
     }
-    void recursion(int col,vector <vector <string>> &res,vector <string> board)
+    void recursion(int col,vector <vector <string>> &res,vector <string> board,vector <int> &lRow,vector <int> &diagUp,vector <int> &diagDown)
     {
         if(col==board.size())
         {
@@ -51,10 +51,16 @@ public:
         for(int i=0;i<board.size();i++)
         {
             // cout<<col<<" "<<i;
-            if(isSafe(i,col,board))
+            if(lRow[i]==0 && diagDown[i+col]==0 && diagUp[board.size()-1+col-i]==0)
             {
                 board[i][col]='Q';
-                recursion(col+1,res,board);
+                lRow[i]=1;
+                diagDown[i+col]=1;
+                diagUp[board.size()-1+col-i]=1;
+                recursion(col+1,res,board,lRow,diagUp,diagDown);
+                lRow[i]=0;
+                diagDown[i+col]=0;
+                diagUp[board.size()-1+col-i]=0;
                 board[i][col]='.';
             }
         }
@@ -65,7 +71,9 @@ public:
         string s(n,'.');
         for(int i=0;i<n;i++)
             board[i]=s;
-        recursion(0,res,board);
+        vector <int> lRow(n,0),diagUp(2*n-1,0),diagDown(2*n-1,0);
+        recursion(0,res,board,lRow,diagUp,diagDown);
+        
         return res;
     }
 };
