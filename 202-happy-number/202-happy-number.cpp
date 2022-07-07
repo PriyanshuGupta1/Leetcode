71379ca8009745dd06e7bfef61467df87124d977
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-07 06:26:48 +0530

    Time: 4 ms (45.75%), Space: 6.1 MB (52.13%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n==1)
+            return true;
+        int curr=n,prev=1;
+        map <long long ,long long> mp;
+        while(n!=1)
+        {
+            cout<<curr<<" ";
+            mp[curr]=1;
+            if(prev==curr)
+                break;
+            prev=curr;
+            int num=curr,sum=0;
+            while(num>0)
+            {
+                int dig=num%10;
+                sum+=dig*dig;
+                num/=10;
+            }
+            
+            curr=sum;
+            if(mp[curr])
+                break;
+        }
+        return curr==1 ? true :false;
+    }
+};
