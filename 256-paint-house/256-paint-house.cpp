eced52181611b7ae59b09faf783a11b742d31f3e
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-08 07:52:04 +0530

    Time: 10 ms (75.39%), Space: 10.3 MB (17.68%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -17,8 +17,10 @@ public:
     int minCost(vector<vector<int>>& costs) {
         // we will start form 0th index
         // we also need to check what we picked in previous iteration ,that cant be picked in the current iteration
-        // 0 1 2
-        // 0 1 2 3
+        // 0 1 2 old indexing
+        // 0 1 2 3 new indexing ,0 represents no color
+        // TimeComplexity: O(N*3*2) = O(N)
+        // Space Complexity: O(N*3)= O(N)
         vector <vector <int>> dp(costs.size()+1,vector <int>(costs[0].size()+2,-1));
         return recursion(0,0,costs,dp);
     }
