1b4c05c03be11eb0fbc0da4590c85f182f63aa27
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-08 10:40:43 +0530

    Time: 17 ms (27.87%), Space: 9.6 MB (56.81%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -3,18 +3,19 @@ public:
     int minCost(vector<vector<int>>& cost) {
         // TimeComplexity: O(N*3*2) = O(N)
         // Space Complexity: O(N*3)= O(N)
-        vector <vector <int>> dp(cost.size()+1,vector <int>(cost[0].size()+2,0));
+        vector <int> prev(3,0),curr(3,0);
         for (int j=0;j<3;j++)
-            dp[0][j]=cost[0][j];
+            prev[j]=cost[0][j];
         for(int i=1;i<cost.size();i++)
         {
-            dp[i][0]=cost[i][0]+min(dp[i-1][1],dp[i-1][2]);
-            dp[i][1]=cost[i][1]+min(dp[i-1][0],dp[i-1][2]);
-            dp[i][2]=cost[i][2]+min(dp[i-1][0],dp[i-1][1]);
+            curr[0]=cost[i][0]+min(prev[1],prev[2]);
+            curr[1]=cost[i][1]+min(prev[0],prev[2]);
+            curr[2]=cost[i][2]+min(prev[0],prev[1]);
+            prev=curr;
         }
         int mini=INT_MAX;
         for (int j=0;j<3;j++)
-            mini=min(mini,dp[cost.size()-1][j]);
+            mini=min(mini,prev[j]);
         return mini;
     }
 };
