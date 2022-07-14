53882ca22c2db69448054976154644f1c7299ec5
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-14 08:59:02 +0530

    Time: 44 ms (74.08%), Space: 18 MB (18.59%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector <vector <char>> &grid)
-    {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0' || grid[i][j]=='2')
-            return;
-        grid[i][j]='2';
-        dfs(i-1,j,grid);
-        dfs(i+1,j,grid);
-        dfs(i,j+1,grid);
-        dfs(i,j-1,grid);
-    }
     int numIslands(vector<vector<char>>& grid) {
         int numOfIsland=0;
         for(int i=0;i<grid.size();i++)
@@ -19,7 +9,36 @@ public:
                 if(grid[i][j]=='1')
                 {
                     numOfIsland++;
-                    dfs(i,j,grid);
+                    queue <pair <int,int>> q;
+                    q.push({i,j});
+                    grid[i][j]='2';
+                    while(!q.empty())
+                    {
+                        auto top=q.front();
+                        q.pop();
+                        int r=top.first,c=top.second;
+                        
+                        if(r>0 && grid[r-1][c]=='1')
+                        {
+                            q.push({r-1,c});
+                            grid[r-1][c]='2';
+                        }
+                        if(r<grid.size()-1 && grid[r+1][c]=='1')
+                        {
+                            q.push({r+1,c});
+                            grid[r+1][c]='2';
+                        }
+                        if(c>0 && grid[r][c-1]=='1')
+                        {
+                            q.push({r,c-1});
+                            grid[r][c-1]='2';
+                        }
+                        if(c<grid[0].size()-1 && grid[r][c+1]=='1')
+                        {
+                            q.push({r,c+1});
+                            grid[r][c+1]='2';
+                        }
+                    }
                 }
             }
         }
