efabaa64b600663f3a1db59a091c0c09e4e97529
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-15 07:50:13 +0530

    Time: 103 ms (88.27%), Space: 17.8 MB (53.83%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int dfs(int x,int y,int r,vector <vector <int> > &bombs,vector <int> &vis)
+    int dfs(int index,vector <int> &vis,vector <int> adj[])
     {
         int currBomb=0;
-        for(int i=0;i<bombs.size();i++)
+        for(auto it : adj[index]) 
         {
-            // cout<<"I"<<i<<endl;
-            if(vis[i] != 1)
+            if(vis[it] != 1)
             {
-                long long x1=bombs[i][0],y1=bombs[i][1];
-                long double xt=sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
-                if(xt <= r)
-                {
-                    currBomb++;
-                    vis[i]=1;
-                    currBomb+=dfs(bombs[i][0],bombs[i][1],bombs[i][2],bombs,vis);
-                }
+                currBomb++;
+                vis[it]=1;
+                currBomb+=dfs(it,vis,adj);
             }
         }
         // cout<<"CurrBomb"<<currBomb<<endl;
@@ -25,11 +19,26 @@ public:
         // explode each bomb find the bomb that causes most destruction
         // by exploding a bomb explode all the bomb in range of this bomb
         int maxBomb=1;
+        vector <int> adj[bombs.size()];
+        for(int i=0;i<bombs.size();i++)
+        {
+            int x=bombs[i][0],y=bombs[i][1],r=bombs[i][2];
+            for(int j=0;j<bombs.size();j++)
+            {
+                if(i!=j)
+                {
+                    long long x1=bombs[j][0],y1=bombs[j][1];
+                    long double xt=sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
+                    if(xt <= r )
+                        adj[i].push_back(j);
+                }
+            }
+        }
         for(int i=0;i<bombs.size();i++)
         {
             vector <int> vis(bombs.size(),0);
             vis[i]=1;
-            maxBomb=max(maxBomb,1+dfs(bombs[i][0],bombs[i][1],bombs[i][2],bombs,vis));
+            maxBomb=max(maxBomb,1+dfs(i,vis,adj));
             // cout<<"MaxBomb"<<maxBomb<<endl;
             vis[i]=0;
         }
