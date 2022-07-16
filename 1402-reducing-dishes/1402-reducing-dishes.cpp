b74dde40300c71f782a5b966b0111167f9ae77ff
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-16 09:11:34 +0530

    Time: 3 ms (91.72%), Space: 8 MB (71.72%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int recursion(int index,int time,vector <int> &satisfaction,vector <vector <int>> &dp)
-    {
-        if(index == satisfaction.size() )
-            return 0;
-        if(dp[index][time] != -1)
-            return dp[index][time];
-        int pick = time*satisfaction[index]+recursion(index+1 ,time+1,satisfaction,dp);
-        int notpick=recursion(index+1,time,satisfaction,dp);
-        return dp[index][time]=max(pick,notpick);
-    }
     int maxSatisfaction(vector<int>& satisfaction) {
         // sort the array 
-        // apply dp on it
         sort(satisfaction.begin(),satisfaction.end());
-        vector <vector <int>> dp(satisfaction.size()+1,vector <int> (satisfaction.size()+1,-1));
-        return recursion(0,1,satisfaction,dp);
+        int cumSum=0;
+        int ans=0;
+        int cur=0;
+        for(int i = satisfaction.size()-1;i>=0;i--)
+        {
+            cur += cumSum+satisfaction[i];
+            cumSum+=satisfaction[i];
+            ans=max(ans,cur);
+        }
+        return ans;
     }
 };
