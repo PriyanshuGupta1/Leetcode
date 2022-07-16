7715c0ad6566d445a7c89608f925ec07db5d6e76
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-16 17:53:34 +0530

    Time: 143 ms (5.02%), Space: 16.3 MB (5.24%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void bfs(int i,vector <int> adj[], vector <int> &vis)
+    {
+        queue <int> q;
+        q.push(i);
+        while( !q.empty() )
+        {
+            int top=q.front();
+            q.pop();
+            vis[top]=1;
+            for( auto it: adj[top] )
+            {
+                if( vis[it] != 1)
+                {
+                    q.push(it);
+                }
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        // we have to return the different number of components
+        vector <int> adj[isConnected.size()+1];
+        for(int i=0;i<isConnected.size();i++)
+        {
+            for(int j=0;j<isConnected[0].size();j++)
+            {
+                if( i != j )
+                {
+                    if(isConnected[i][j] == 1)
+                    {
+                        adj[i].push_back(j);
+                        adj[j].push_back(i);
+                    }
+                }
+            }
+        }
+        vector <int> vis(isConnected.size()+1,0);
+        int count=0;
+        for(int i=0;i<isConnected.size();i++)
+        {
+            if(vis[i] != 1)
+            {
+                bfs(i,adj,vis);
+                count++;
+            }
+        }
+        return count;
+    }
+};
