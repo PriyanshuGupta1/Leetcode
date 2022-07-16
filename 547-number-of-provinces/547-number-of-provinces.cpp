3301a9f200457c0216e609988a774b4eeaec3aa8
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-16 17:59:32 +0530

    Time: 56 ms (15.90%), Space: 14.4 MB (28.43%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void bfs(int i,vector <int> adj[], vector <int> &vis)
+    void bfs(int i,vector<vector<int>>& isConnected, vector <int> &vis)
     {
         queue <int> q;
         q.push(i);
@@ -9,35 +9,23 @@ public:
             int top=q.front();
             q.pop();
             vis[top]=1;
-            for( auto it: adj[top] )
-                if( vis[it] != 1)
-                    q.push(it);
+            for(int j=0;j<isConnected.size();j++)
+            {
+                if( vis[j] != 1 && top!=j && isConnected[top][j] == 1)
+                    q.push(j);
+            }
+                
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         // we have to return the different number of components
-        vector <int> adj[isConnected.size()+1];
-        for(int i=0;i<isConnected.size();i++)
-        {
-            for(int j=0;j<isConnected[0].size();j++)
-            {
-                if( i != j )
-                {
-                    if(isConnected[i][j] == 1)
-                    {
-                        adj[i].push_back(j);
-                        adj[j].push_back(i);
-                    }
-                }
-            }
-        }
         vector <int> vis(isConnected.size()+1,0);
         int count=0;
         for(int i=0;i<isConnected.size();i++)
         {
             if(vis[i] != 1)
             {
-                bfs(i,adj,vis);
+                bfs(i,isConnected,vis);
                 count++;
             }
         }
