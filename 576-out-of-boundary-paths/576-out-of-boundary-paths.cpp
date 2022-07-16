69996d14127ac4e62b87aef6db3329e415b79df1
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-16 07:58:20 +0530

    Time: 16 ms (51.97%), Space: 11.4 MB (13.31%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long mod=10e8+7;
+    int recursion(int i,int j,int m,int n,int maxMove,vector <vector <vector <long>>> &dp)
+    {
+        if(maxMove<0)
+            return 0;
+        if(i<0 || j<0 || i==m || j==n)
+            return 1;
+        if(dp[i][j][maxMove] != -1)
+            return dp[i][j][maxMove];
+        return dp[i][j][maxMove]=(recursion(i+1,j,m,n,maxMove-1,dp)%mod+recursion(i,j+1,m,n,maxMove-1,dp)%mod+recursion(i-1,j,m,n,maxMove-1,dp)%mod+recursion(i,j-1,m,n,maxMove-1,dp)%mod)%mod;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector <vector <vector<long>>> dp(m+1,vector <vector<long>> (n+1,vector <long>(maxMove+1,-1)));
+        return dp[startRow][startColumn][maxMove]=recursion(startRow,startColumn,m,n,maxMove,dp);
+    }
+};
