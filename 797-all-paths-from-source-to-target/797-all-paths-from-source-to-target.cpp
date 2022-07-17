386b4a50f5c1688c96b68797849252b22834cd0e
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-17 18:18:58 +0530

    Time: 44 ms (24.32%), Space: 18.5 MB (12.67%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dfs(int index,vector<vector<int>>& graph,vector <int> dfsVis,vector <vector<int>> &res,vector <int> curr)
+    {
+        if(index==graph.size()-1)
+        {
+            curr.push_back(index);
+            res.push_back(curr);
+            return;
+        }
+        // cout<<index<<endl;
+        dfsVis[index]=1;
+        curr.push_back(index);
+        for(auto it:graph[index])
+        {
+            // cout<<it<<" ";
+            if(dfsVis[it] !=1)
+            {
+                dfs(it,graph,dfsVis,res,curr);
+            }
+        }
+        dfsVis[index]=0;
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        // adjacency matrix is given
+        // dfs traversal take each route if we find one route to be successfull we will store it in res
+        // we will traverse all the path originating from root 
+        vector <int> curr;
+        vector <vector <int>> res;
+        vector <int> dfsVis(graph.size()+1,0);
+        dfs(0,graph,dfsVis,res,curr);
+        return res;
+    }
+};
