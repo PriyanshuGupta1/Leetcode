2a9b5bd568fe6ac9ab3872251411554ad25a2e8a
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-19 18:55:53 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (29.13%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // both are sorted arrays
+        // we will use two pointers to 
+        int j=0;
+        for(int i=m;i<m+n;i++)
+            nums1[i]=nums2[j++];
+        sort(nums1.begin(),nums1.end());
+    }
+};
