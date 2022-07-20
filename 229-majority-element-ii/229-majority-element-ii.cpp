0d3fbd016e43cd2cdf6e0484256cca27f9df68d2
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-07-20 10:32:11 +0530

    Time: 25 ms (32.59%), Space: 15.9 MB (11.87%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,13 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map <int,int> umap;
         vector <int> res;
+        int count1=0,count2=0,majEle1=INT_MAX,majEle2=INT_MAX;
         for(int i=0;i<nums.size();i++)
-            umap[nums[i]]++;
-        for(auto it=umap.begin();it!=umap.end();it++)
-            if(it->second>nums.size()/3)
-                res.push_back(it->first);
+        {
+            if(count1 == 0 && majEle2 !=nums[i])
+            {
+                majEle1=nums[i];
+                count1++;
+                continue;
+            }
+            if(count2 == 0 && majEle1!=nums[i])
+            {
+                majEle2=nums[i];
+                count2++;
+                continue;
+            }
+            if(majEle1!=nums[i] && majEle2!=nums[i])
+            {
+                count1--;
+                count2--;
+            }
+            else if(majEle1==nums[i])
+                count1++;
+            else if(majEle2==nums[i])
+                count2++;
+        }
+        count1=0,count2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==majEle1)
+                count1++;
+            if(nums[i]==majEle2)
+                count2++;
+        }
+        if(count1>nums.size()/3)
+            res.push_back(majEle1);
+        if(count2>nums.size()/3)
+            res.push_back(majEle2);
         return res;
     }
 };
