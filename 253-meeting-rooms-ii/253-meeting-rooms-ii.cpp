ae533dceb246d4b5f356d6a068ab7f24f970b658
Priyanshu Gupta <55499361+PriyanshuGupta1@users.noreply.github.com>
2022-08-04 15:07:37 +0530

    Time: 14 ms (97.09%), Space: 12.1 MB (53.59%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        int maxi=0;
-        for(int i=0;i<intervals.size();i++)
-            maxi=max(maxi,intervals[i][1]);
-        vector <int> room(maxi+2,0);
+        // Naive approach
+        // int maxi=0;
+        // for(int i=0;i<intervals.size();i++)
+        //     maxi=max(maxi,intervals[i][1]);
+        // vector <int> room(maxi+2,0);
+        // for(int i=0;i<intervals.size();i++)
+        // {
+        //     room[intervals[i][0]]++;
+        //     room[intervals[i][1]] --;
+        // }
+        // int maxRoom=0,sum=0;
+        // for(int i=0;i<room.size();i++)
+        // {
+        //     sum+=room[i];
+        //     maxRoom=max(sum,maxRoom);
+        // }
+        // return maxRoom;
+        vector <int> arrival;
+        vector <int> dept;
         for(int i=0;i<intervals.size();i++)
         {
-            room[intervals[i][0]]++;
-            room[intervals[i][1]] --;
+            arrival.push_back(intervals[i][0]);
+            dept.push_back(intervals[i][1]);
         }
-        int maxRoom=0,sum=0;
-        for(int i=0;i<room.size();i++)
+        sort(arrival.begin(),arrival.end());
+        sort(dept.begin(),dept.end());
+        int i=0,j=0,plat=0,count=0;
+        while( i < intervals.size() )
         {
-            sum+=room[i];
-            maxRoom=max(sum,maxRoom);
+            while( dept[j] <= arrival[i] )
+            {
+                j++;
+                plat--;
+            }
+            i++;
+            plat++;
+            count=max(count,plat);
         }
-        return maxRoom;
+        return count;
     }
 };
